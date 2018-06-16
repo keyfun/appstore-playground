08fccb12074fe772d3fc73f4d06939df591ad758
Key Hui <>
2018-06-16 19:00:44 +0800

    update layout for grossing app

--- AppStorePlayground/View/GrossingAppViewCell.swift
+++ AppStorePlayground/View/GrossingAppViewCell.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 final class GrossingAppViewCell: UICollectionViewCell {
+
     @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var lbCategory: UILabel!
     @IBOutlet weak var ivImage: UIImageView!
-    
+
+    private var lbNameFrame: CGRect!
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        ivImage.layer.cornerRadius = 16.0
+        ivImage.clipsToBounds = true
+        lbNameFrame = lbName.frame
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-//        lbName.sizeToFit()
+        
+        // align label text to top center
+        lbName.frame = lbNameFrame
+        lbName.sizeToFit()
+        let newFrame = lbName.frame
+        lbName.frame = CGRect(
+            x: lbNameFrame.origin.x,
+            y: newFrame.origin.y,
+            width: lbNameFrame.width,
+            height: newFrame.height)
     }
+
 }
